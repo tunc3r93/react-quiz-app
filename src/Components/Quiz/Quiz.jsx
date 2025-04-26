6b27e5601bb811187dffc9751c3500e1f29470d5
tuncer <tuncer.dev@mail.de>
2025-04-26 23:39:17 +0200

    feat: implement next question functionality triggered by answer selection and next button click

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -6,6 +6,7 @@ const Quiz = () => {
     let [index, setIndex] = useState(0);
     let [question, setQuestion] = useState(data[index]);
     let [lock, setLock] = useState(false);
+    let [score, setScore] = useState(0);
 
     let Option1 = useRef(null);
     let Option2 = useRef(null);
@@ -19,11 +20,24 @@ const Quiz = () => {
             if(ans === question.ans) {
                 e.target.classList.add("correct");
                 setLock(true);
+                setScore(prev => prev + 1);
             }else{
                 e.target.classList.add("wrong");
                 setLock(true);
                 option_array[question.ans - 1].current.classList.add("correct");
             }
+        }
+    }
+
+    const next = () => {
+        if(lock === true){
+            setIndex(++index);
+            setQuestion(data[index]);
+            setLock(false);
+            option_array.map((option) => {
+                option.current.classList.remove("correct");
+                option.current.classList.remove("wrong");
+            })
         }
     }
 
@@ -38,7 +52,7 @@ const Quiz = () => {
                 <li ref={Option3} onClick = {(e) => {checkAnswer(e, 3)}}>{ question.option3 }</li>
                 <li ref={Option4} onClick = {(e) => {checkAnswer(e, 4)}}>{ question.option4 }</li>
             </ul>
-            <button>Next</button>
+            <button onClick={() => next()}>Next</button>
             <div className="index">
                 1 of 5 questions
             </div>
