b39d0d9af6f382565997f1112ca07767500e2f5e
tuncer <tuncer.dev@mail.de>
2025-04-26 23:18:23 +0200

    feat: implement onClick event for li tags to provide dynamic feedback with color changes for correct/incorrect answers

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -6,16 +6,24 @@ const Quiz = () => {
     let [index, setIndex] = useState(0);
     let [question, setQuestion] = useState(data[index]);
 
+    const checkAnswer = (e, ans) => {
+        if(ans === question.ans) {
+            e.target.classList.add("correct");
+        }else{
+            e.target.classList.add("wrong");
+        }
+    }
+
     return (
         <div className="container">
             <h1>Quiz App</h1>
             <hr />
             <h2>{index + 1} . {question.question}</h2>
             <ul>
-                <li>{ question.option1 }</li>
-                <li>{ question.option2 }</li>
-                <li>{ question.option3 }</li>
-                <li>{ question.option4 }</li>
+                <li onClick = {(e) => {checkAnswer(e, 1)}}>{ question.option1 }</li>
+                <li onClick = {(e) => {checkAnswer(e, 2)}}>{ question.option2 }</li>
+                <li onClick = {(e) => {checkAnswer(e, 3)}}>{ question.option3 }</li>
+                <li onClick = {(e) => {checkAnswer(e, 4)}}>{ question.option4 }</li>
             </ul>
             <button>Next</button>
             <div className="index">
