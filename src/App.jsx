9ec0b879e0cc47aa0f902e9b980c14e2bf7bb2c3
tuncer <tuncer.dev@mail.de>
2025-04-26 18:17:32 +0200

    feat: implement Quiz component and display in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Quiz from './Components/Quiz/Quiz.jsx';
 
 const App = () => {
     return (
         <div>
-
+            <Quiz />
         </div>
     )
 };
---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Quiz.css";
+
+const Quiz = () => {
+    return (
+        <div className="container">
+            <h1>Quiz App</h1>
+        </div>
+    )
+}
+
+export default Quiz;
