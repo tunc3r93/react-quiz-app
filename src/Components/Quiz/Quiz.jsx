e3c730ac326af2d371736869c632a0348485bde6
tuncer <tuncer.dev@mail.de>
2025-04-26 21:59:46 +0200

    feat: add HTML hyperlink question

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -5,6 +5,18 @@ const Quiz = () => {
     return (
         <div className="container">
             <h1>Quiz App</h1>
+            <hr />
+            <h2>Which HTML tag is used to create a hyperlink?</h2>
+            <ul>
+                <li>&lt;link&gt;</li>
+                <li>&lt;a&gt;</li>
+                <li>&lt;href&gt;</li>
+                <li>&lt;url&gt;</li>
+            </ul>
+            <button>Next</button>
+            <div className="index">
+                1 of 5 questions
+            </div>
         </div>
     )
 }
